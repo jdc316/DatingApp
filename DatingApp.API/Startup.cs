47a72c0a57727670a27096e4e6a891db6354ee4a
Teslajcunningham <jcunningham@teslaforecast.com>
2019-11-18 08:14:33 -0500

    Done for today

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
